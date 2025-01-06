63c6e2bb26c7b59647e69d07b8a4aac308746b9a
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-01-05 16:47:09 -0800

    Create a file and import all the libraries required by the program

---
+++ app.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    cout << "wellcome to this game...";
+    return 0;
+}
