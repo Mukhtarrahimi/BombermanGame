fd77561a85d3696e176097a793b03a723196da10
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-01-05 16:58:13 -0800

    Writing down the important elements of this game

--- app.cpp
+++ app.cpp
@@ -7,6 +7,25 @@
 
 using namespace std;
 
+// عناصر مورد نیاز بازی
+enum class TileType
+{
+    Empty,
+    Concrete,
+    Brick,
+    Player,
+    Enemy,
+    Exit,
+    Bomb
+};
+
+enum class SkillType
+{
+    None,
+    Remote,
+    ExplosionRadius
+};
+
 int main()
 {
     cout << "wellcome to this game...";
