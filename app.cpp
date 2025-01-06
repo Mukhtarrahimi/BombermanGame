0f881988c532a777603c1a024a3bda5898f67132
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-01-05 17:35:56 -0800

    Defining public variables

--- app.cpp
+++ app.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 // عناصر مورد نیاز بازی
+
 enum class TileType
 {
     Empty,
@@ -18,7 +19,6 @@ enum class TileType
     Exit,
     Bomb
 };
-
 enum class SkillType
 {
     None,
@@ -26,6 +26,38 @@ enum class SkillType
     ExplosionRadius
 };
 
+const int BOARD_SIZE = 10;
+
+TileType board[BOARD_SIZE][BOARD_SIZE]; // ساخت ارایه برای تخته بازی
+
+// موقعیت بازیکن
+int playerX = 0, playerY = 0;
+
+// امتیاز بازی و تعداد حرکت‌ها
+int score = 0;
+int moves = 0;
+int bombsUsed = 0;
+
+// وضعیت اجرای بازی
+bool isRunning = true;
+
+SkillType currentSkill = SkillType::None;
+
+// شعاع انفجار بمب‌ها
+int explosionRadius = 1;
+
+std::string playerName;
+
+void initializeBoard()
+{
+    for (int i = 0; i < BOARD_SIZE; ++i)
+    {
+        for (int j = 0; j < BOARD_SIZE; ++j)
+        {
+            board[i][j] = TileType::Empty;
+        }
+    }
+}
 int main()
 {
     cout << "wellcome to this game...";
