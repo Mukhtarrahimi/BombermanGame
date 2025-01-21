cb3e9e0cb123ba4388fb87ee09b4747166582fb5
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-01-21 08:32:43 -0800

    Update code for the bomb part of the game

--- app.cpp
+++ app.cpp
@@ -15,7 +15,7 @@ enum class Elements
 {
     Empty,
     Concrete,
-    Brick,
+    Wall,
     Player,
     Enemy,
     Exit,
@@ -51,54 +51,15 @@ SkillType currentSkill = SkillType::None;
 int explosionRadius = 1;
 string playerName;
 
-struct Bomb
-{
-    int x, y;
-    int remainingMoves;
-
-    Bomb() : x(-1), y(-1), remainingMoves(0) {}
-
-    Bomb(int x, int y) : x(x), y(y), remainingMoves(2) {}
-
-    void move()
-    {
-        if (remainingMoves > 0)
-        {
-            remainingMoves--;
-        }
-        if (remainingMoves == 0)
-        {
-            explode();
-        }
-    }
-
-    void explode()
-    {
-        cout << "Bomb exploded at (" << x << ", " << y << ")" << endl;
-        for (int i = -explosionRadius; i <= explosionRadius; i++)
-        {
-            for (int j = -explosionRadius; j <= explosionRadius; j++)
-            {
-                int nx = x + i, ny = y + j;
-                if (nx >= 0 && nx < boardSize && ny >= 0 && ny < boardSize)
-                {
-                    if (board[ny][nx] == Elements::Brick || board[ny][nx] == Elements::Enemy)
-                    {
-                        board[ny][nx] = Elements::Empty;
-                    }
-                }
-            }
-        }
-    }
-};
-
-Bomb bombs[maxBomb];
-int bombCount = 0;
-
 void setColor(int color)
 {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
+// حذف ساختار struct برای بمب‌ها
+int bombsX[maxBomb];
+int bombsY[maxBomb];
+int bombRemainingMoves[maxBomb];
+int bombCount = 0;
 
 void placeBomb()
 {
@@ -125,7 +86,9 @@ void placeBomb()
 
     if (validPosition)
     {
-        bombs[bombCount] = Bomb(x, y);
+        bombsX[bombCount] = x;
+        bombsY[bombCount] = y;
+        bombRemainingMoves[bombCount] = 2; // مدت زمان فعال بودن بمب
         bombCount++;
         bombsUsed++;
         board[y][x] = Elements::Bomb;
@@ -134,6 +97,26 @@ void placeBomb()
     else
     {
         cout << "No suitable position for bomb found. Try again.\n";
+    }
+}
+
+void explodeBomb(int bombIndex)
+{
+    int x = bombsX[bombIndex], y = bombsY[bombIndex];
+    cout << "Bomb exploded at (" << x << ", " << y << ")" << endl;
+    for (int i = -explosionRadius; i <= explosionRadius; i++)
+    {
+        for (int j = -explosionRadius; j <= explosionRadius; j++)
+        {
+            int nx = x + i, ny = y + j;
+            if (nx >= 0 && nx < boardSize && ny >= 0 && ny < boardSize)
+            {
+                if (board[ny][nx] == Elements::Wall || board[ny][nx] == Elements::Enemy)
+                {
+                    board[ny][nx] = Elements::Empty;
+                }
+            }
+        }
     }
 }
 
@@ -164,7 +147,7 @@ void printBoard()
                 setColor(8); // Dark gray text for concrete
                 cout << " ## ";
                 break;
-            case Elements::Brick:
+            case Elements::Wall:
                 setColor(14); // Yellow for brick
                 cout << " -_ ";
                 break;
@@ -200,7 +183,7 @@ void generateGameElements()
         int x = rand() % boardSize;
         int y = rand() % boardSize;
         if (board[y][x] == Elements::Empty)
-            board[y][x] = Elements::Brick;
+            board[y][x] = Elements::Wall;
 
         x = rand() % boardSize;
         y = rand() % boardSize;
@@ -367,7 +350,7 @@ void adjustGameForDifficulty()
         int x = rand() % boardSize;
         int y = rand() % boardSize;
         if (board[y][x] == Elements::Empty)
-            board[y][x] = Elements::Brick;
+            board[y][x] = Elements::Wall;
     }
 
     for (int i = 0; i < enemyCount; i++)
@@ -429,6 +412,386 @@ void showScoreboard()
     }
 }
 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <windows.h> // برای استفاده از SetConsoleTextAttribute
+
+using namespace std;
+using namespace std::chrono;
+
+steady_clock::time_point gameStartTime, gameEndTime;
+
+enum class Elements
+{
+    Empty,
+    Concrete,
+    Wall,
+    Player,
+    Enemy,
+    Exit,
+    Bomb
+};
+
+enum class Difficulty
+{
+    Easy,
+    Medium,
+    Hard
+};
+
+Difficulty currentDifficulty = Difficulty::Medium;
+
+const int boardSize = 10;
+const int maxBomb = 10;
+
+Elements board[boardSize][boardSize];
+int playerX = 0, playerY = 0;
+int score = 0;
+int moves = 0;
+int bombsUsed = 0;
+bool isRunning = true;
+int bombCount = 0;
+
+// تغییرات: استفاده از آرایه‌ها به جای ساختار Bomb
+int bombsX[maxBomb];
+int bombsY[maxBomb];
+int bombRemainingMoves[maxBomb];
+
+void setColor(int color)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void placeBomb()
+{
+    if (bombCount >= maxBomb)
+    {
+        cout << "Maximum number of bombs reached!\n";
+        return;
+    }
+
+    int x = playerX, y = playerY;
+    bool validPosition = false;
+
+    for (int attempts = 0; attempts < 10; attempts++)
+    {
+        x = rand() % boardSize;
+        y = rand() % boardSize;
+
+        if (board[y][x] == Elements::Empty)
+        {
+            validPosition = true;
+            break;
+        }
+    }
+
+    if (validPosition)
+    {
+        bombsX[bombCount] = x;
+        bombsY[bombCount] = y;
+        bombRemainingMoves[bombCount] = 2; // مدت زمان فعال بودن بمب
+        bombCount++;
+        bombsUsed++;
+        board[y][x] = Elements::Bomb;
+        cout << "Bomb placed at (" << x << ", " << y << ")\n";
+    }
+    else
+    {
+        cout << "No suitable position for bomb found. Try again.\n";
+    }
+}
+
+void moveBombs()
+{
+    for (int i = 0; i < bombCount; i++)
+    {
+        if (bombRemainingMoves[i] > 0)
+        {
+            bombRemainingMoves[i]--;
+        }
+    }
+}
+
+void explodeBomb(int index)
+{
+    int x = bombsX[index], y = bombsY[index];
+    if (board[y][x] == Elements::Bomb)
+    {
+        board[y][x] = Elements::Empty;
+        cout << "Bomb at (" << x << ", " << y << ") exploded!\n";
+        score -= 100; // کاهش امتیاز به ازای انفجار بمب
+        bombCount--;
+    }
+}
+
+void handleBombs()
+{
+    for (int i = 0; i < bombCount; i++)
+    {
+        if (bombRemainingMoves[i] <= 0)
+        {
+            explodeBomb(i);
+        }
+    }
+}
+
+void initializeBoard()
+{
+    for (int y = 0; y < boardSize; y++)
+    {
+        for (int x = 0; x < boardSize; x++)
+        {
+            board[y][x] = Elements::Empty;
+        }
+    }
+}
+
+void printBoard()
+{
+    for (int y = 0; y < boardSize; y++)
+    {
+        for (int x = 0; x < boardSize; x++)
+        {
+            switch (board[y][x])
+            {
+            case Elements::Empty:
+                setColor(7); // White text for empty
+                cout << " - ";
+                break;
+            case Elements::Concrete:
+                setColor(8); // Dark gray text for concrete
+                cout << " ## ";
+                break;
+            case Elements::Wall:
+                setColor(14); // Yellow for brick
+                cout << " -_ ";
+                break;
+            case Elements::Player:
+                setColor(10); // Green for player
+                cout << " SS ";
+                break;
+            case Elements::Enemy:
+                setColor(12); // Red for enemy
+                cout << " EE ";
+                break;
+            case Elements::Exit:
+                setColor(11); // Cyan for exit
+                cout << " <> ";
+                break;
+            case Elements::Bomb:
+                setColor(13); // Magenta for bomb
+                cout << " Bo ";
+                break;
+            }
+        }
+        cout << endl;
+    }
+
+    cout << "Score: " << score << " | Moves: " << moves << " | Bombs Used: " << bombsUsed << endl;
+}
+
+void generateGameElements()
+{
+    srand(time(0));
+    for (int i = 0; i < boardSize / 2; i++)
+    {
+        int x = rand() % boardSize;
+        int y = rand() % boardSize;
+        if (board[y][x] == Elements::Empty)
+            board[y][x] = Elements::Wall;
+
+        x = rand() % boardSize;
+        y = rand() % boardSize;
+        if (board[y][x] == Elements::Empty)
+            board[y][x] = Elements::Enemy;
+
+        x = rand() % boardSize;
+        y = rand() % boardSize;
+        if (board[y][x] == Elements::Empty)
+            board[y][x] = Elements::Concrete;
+    }
+    board[boardSize - 1][boardSize - 1] = Elements::Exit;
+    board[playerY][playerX] = Elements::Player;
+}
+
+void saveGame(const string &filename)
+{
+    ofstream outFile(filename);
+    if (outFile.is_open())
+    {
+        outFile << playerX << " " << playerY << endl;
+        outFile << score << " " << moves << " " << bombsUsed << endl;
+        for (int y = 0; y < boardSize; y++)
+        {
+            for (int x = 0; x < boardSize; x++)
+            {
+                outFile << static_cast<int>(board[y][x]) << " ";
+            }
+            outFile << endl;
+        }
+
+        outFile << bombCount << endl;
+        for (int i = 0; i < bombCount; i++)
+        {
+            outFile << bombsX[i] << " " << bombsY[i] << " " << bombRemainingMoves[i] << endl;
+        }
+
+        outFile.close();
+        cout << "Game saved successfully!" << endl;
+    }
+}
+
+void loadGame(const string &filename)
+{
+    ifstream inFile(filename);
+    if (inFile.is_open())
+    {
+        inFile >> playerX >> playerY;
+        inFile >> score >> moves >> bombsUsed;
+        for (int y = 0; y < boardSize; y++)
+        {
+            for (int x = 0; x < boardSize; x++)
+            {
+                int temp;
+                inFile >> temp;
+                board[y][x] = static_cast<Elements>(temp);
+            }
+        }
+
+        inFile >> bombCount;
+        for (int i = 0; i < bombCount; i++)
+        {
+            inFile >> bombsX[i] >> bombsY[i] >> bombRemainingMoves[i];
+        }
+
+        inFile.close();
+        cout << "Game loaded successfully!" << endl;
+    }
+    else
+    {
+        cout << "Invalid save file or file not found. Starting a new game.\n";
+        initializeBoard();
+        generateGameElements();
+    }
+}
+
+void movePlayer(char direction)
+{
+    board[playerY][playerX] = Elements::Empty;
+
+    int oldX = playerX, oldY = playerY;
+
+    if ((direction == 'W' || direction == 'w') && playerY > 0)
+        playerY--;
+    if ((direction == 'S' || direction == 's') && playerY < boardSize - 1)
+        playerY++;
+    if ((direction == 'A' || direction == 'a') && playerX > 0)
+        playerX--;
+    if ((direction == 'D' || direction == 'd') && playerX < boardSize - 1)
+        playerX++;
+
+    if (board[playerY][playerX] == Elements::Enemy)
+    {
+        cout << "You encountered an enemy!\n";
+        score += 100;
+        board[playerY][playerX] = Elements::Empty;
+    }
+
+    if (board[playerY][playerX] == Elements::Concrete)
+    {
+        cout << "You hit a wall!\n";
+        playerX = oldX;
+        playerY = oldY;
+    }
+
+    board[playerY][playerX] = Elements::Player;
+    moves++;
+}
+
+void showMenu()
+{
+    cout << "\n1. Start Game\n2. Load Game\n3. Set Difficulty\n4. Guide\n5. Scoreboard\n6. Exit\n";
+    cout << "Enter your choice: ";
+}
+
+void showGuide()
+{
+    cout << "Welcome to the game!\n";
+    cout << "Move with W, A, S, D.\nPlace bombs with B.\n";
+    cout << "Reach the exit to win.\n";
+}
+
+void adjustGameForDifficulty()
+{
+    switch (currentDifficulty)
+    {
+    case Difficulty::Easy:
+        cout << "Setting up game for Easy difficulty.\n";
+        break;
+    case Difficulty::Medium:
+        cout << "Setting up game for Medium difficulty.\n";
+        break;
+    case Difficulty::Hard:
+        cout << "Setting up game for Hard difficulty.\n";
+        break;
+    }
+
+    initializeBoard();
+
+    int enemyCount, brickCount, concreteCount;
+    if (currentDifficulty == Difficulty::Easy)
+    {
+        enemyCount = boardSize * 2;
+        brickCount = boardSize * 3;
+        concreteCount = 6;
+    }
+    else if (currentDifficulty == Difficulty::Medium)
+    {
+        enemyCount = boardSize * 3;
+        brickCount = boardSize * 4;
+        concreteCount = 9;
+    }
+    else
+    {
+        enemyCount = boardSize * 4;
+        brickCount = boardSize * 5;
+        concreteCount = 12;
+    }
+
+    srand(time(0));
+
+    for (int i = 0; i < brickCount; i++)
+    {
+        int x = rand() % boardSize;
+        int y = rand() % boardSize;
+        if (board[y][x] == Elements::Empty)
+            board[y][x] = Elements::Wall;
+    }
+
+    for (int i = 0; i < enemyCount; i++)
+    {
+        int x = rand() % boardSize;
+        int y = rand() % boardSize;
+        if (board[y][x] == Elements::Empty)
+            board[y][x] = Elements::Enemy;
+    }
+
+    for (int i = 0; i < concreteCount; i++)
+    {
+        int x = rand() % boardSize;
+        int y = rand() % boardSize;
+        if (board[y][x] == Elements::Empty)
+            board[y][x] = Elements::Concrete;
+    }
+
+    board[boardSize - 1][boardSize - 1] = Elements::Exit;
+    board[playerY][playerX] = Elements::Player;
+}
+
 int main()
 {
     cout << "Enter player name: ";
@@ -472,7 +835,7 @@ int main()
             showGuide();
             break;
         case 5:
-            showScoreboard();
+            // نمایش اسکوربورد اینجا
             break;
         case 6:
             isRunning = false;
@@ -503,16 +866,14 @@ int main()
                 movePlayer(move);
 
                 // Move bombs
-                for (int i = 0; i < bombCount; i++)
-                {
-                    bombs[i].move();
-                }
+                moveBombs();
+                handleBombs();
 
                 // Check if the game has ended
                 if (board[playerY][playerX] == Elements::Exit)
                 {
                     gameEndTime = steady_clock::now();
-                    calculateScore();
+                    // تابع محاسبه امتیاز می‌تواند در اینجا اضافه شود
                     break;
                 }
             }
